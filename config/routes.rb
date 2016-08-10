ae67d37764d4b8cc3293b9320e27906a3cd7a912
Evan SandersonE <evan.j.sanderson@gmail.coM>
2016-08-10 15:36:01 -0400

    added welcome page and shifted styling

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root "players#index"
+  root "welcome#index"
 
   resources :players do
     resources :decks do
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
     end
   end
 
+  get '/welcome' => "welcome#index"
   get '/decks' => "decks#all_index"
 
 end
